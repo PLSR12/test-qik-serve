754fa3e0e069a11ad59d81705712b22f341615ed
Pedro Lucas Dos Santos <90657983+PLSR12@users.noreply.github.com>
2025-03-04 16:15:42 -0300

    fix: problem vercel

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,12 @@
-import { Routes, Route, Navigate } from "react-router";
+import { Routes, Route } from "react-router";
 import Menu from "../pages/Menu";
 import AuthLayout from "../layouts/AuthLayout";
 
 const Router = () => {
 	return (
 		<Routes>
-			<Route path="/" element={<Navigate to="/menu" replace />} />
-
 			<Route element={<AuthLayout />}>
-				<Route path="/menu" element={<Menu />} />
+				<Route path="/" element={<Menu />} />
 			</Route>
 		</Routes>
 	);
